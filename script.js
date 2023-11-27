cad7a99ecb74e63ef908e06295264e20bfd24f67
Emerson L <emerson.lucirio@yahoo.com.br>
2023-11-27 02:32:57 -0300

    add conteudo

---
+++ script.js
@@ -0,0 +1,9 @@
+var nomeUsuario = "Emerson";
+var valorEmBitcoin = 1;
+var cotacaoEmBitCoin = 176934.00;
+var valorEmReal = (valorEmBitcoin * cotacaoEmBitCoin);
+// valorEmReal = valorEmReal.toFixed(2); // Esta linha não é necessária, pois valorEmReal já está formatado
+
+alert('Olá ' + nomeUsuario + '! O valor em reais é de R$' + valorEmReal);
+ 
+
